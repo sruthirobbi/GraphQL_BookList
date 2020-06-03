16c4777e7439038d6e2941194eab29377fda22fd
SruthiRobbi <sruthidhar.r@gmail.com>
2020-06-03 15:08:52 -0400

    initial commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const app = express();
+
+app.use('/graphql',graphqlHTTP({
+    schema:schema
+}));
+const PORT = process.env.PORT  || 5000;
+
+
+app.listen(PORT, ()=> console.log(`Server Started on port ${PORT}`))
